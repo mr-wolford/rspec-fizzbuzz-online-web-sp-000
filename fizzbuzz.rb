28dacf9d6f978d2d7537d8a2b9e4982a7089a435
Matt Wolford <matthew.r.wolford@gmail.com>
2020-12-27 01:34:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@ def fizzbuzz(int)
   if int % 15 == 0
     puts "FizzBuzz"
   elsif int % 5 == 0
-    puts "Fizz"
-  elsif int % 3 == 0
     puts "Buzz"
+  elsif int % 3 == 0
+    puts "Fizz"
   end
 end
